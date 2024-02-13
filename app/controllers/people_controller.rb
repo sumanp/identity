95fc64beb161ed941b62b03799d5d1eee7040c4a
sumanp <sumanpuri55@gmail.com>
2024-02-13 15:46:17 +0530

    Add person controller & views

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,75 @@
+class PeopleController < ApplicationController
+  before_action :set_person, only: %i[ show edit update destroy ]
+
+  # GET /people or GET /people.json
+  def index
+    @people = Person.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @people }
+    end
+  end
+
+  # GET /people/1 or GET /people/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @person }
+    end
+  end
+
+  # GET /people/new
+  def new
+    @person = Person.new
+  end
+
+  # GET /people/1/edit
+  def edit
+  end
+
+  # POST /people or /people.json
+  def create
+    @person = Person.new(person_params)
+
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to person_url(@person), notice: "Person was successfully created." }
+        format.json { render :show, status: :created, location: @person }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /people/1
+  def update
+    respond_to do |format|
+      if @person.update(person_params)
+        format.html { redirect_to person_url(@person), notice: "Person was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /people/1
+  def destroy
+    @person.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to people_url, notice: "Person was successfully destroyed." }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_person
+      @person = Person.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def person_params
+      params.require(:person).permit(:name)
+    end
+end
